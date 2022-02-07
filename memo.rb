bba60a6e646ab1efe16c202d813b66f83bfe0f55
EC2 Default User <ec2-user@ip-172-31-77-252.ec2.internal>
2022-02-07 13:53:45 +0000

    first submit

--- memo.rb
+++ memo.rb
@@ -1,24 +1,24 @@
 require 'csv'
 puts "1(新規でメモを作成)、2(既存のメモを編集する)"
 memo_type = gets.to_i
-if memo_type == "1"
+if memo_type == 1
   puts "拡張子を除いたファイルを入力してください。"
-  file_name = gets.chomp
+  file_name = gets.chomp + ".csv"
   puts "メモしたい内容を記入してください"
   puts "完了したらCtrl + Dを押してください"
   memo_type = STDIN.read
   
-  CSV.open('test.csv','w') do |memo|
+  CSV.open(file_name,'w') do |memo|
     memo << ["#{memo_type}"]
   end
-else memo_type == "2"
+elsif memo_type == 2
   puts "編集したいファイルを選んでください"
-  file_name = gets.chomp
+  file_name = gets.chomp + ".csv"
   puts "編集内容を入力してください。"
   puts "完了したらCtrl + Dを押してください"
   memo_type = STDIN.read
 
- CSV.open('test.csv', 'a') do |memo|
+ CSV.open(file_name, 'a') do |memo|
    memo << ["#{memo_type}"]
  end
  
